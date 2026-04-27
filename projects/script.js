130d68f8e52e48589b7e60f34a09471aadd87b65
Riya K Alex <riyakalex5@gmail.com>
2026-04-27 18:46:58 +0530

    emailjs integration

--- projects/script.js
+++ projects/script.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("0DnpbYfdNpD3TrWlR");
 
         emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
             .then(function (response) {
